e6f91e327df73b6b80ed2ea16a48ce414b8f92be
duck <duckke@gmail.com>
2022-05-15 12:29:26 +0900

    게임선택 UI 컨트롤하기 위한 스크립트 추가

---
+++ Assets/Scripts/SelectGame/UISelectGame.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISelectGame : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnClickEasy()
+    {
+        Debug.Log("EASY");
+
+    }
+
+    public void OnClickNormal()
+    {
+        Debug.Log("EASY");
+
+    }
+
+    public void OnClickHard()
+    {
+        Debug.Log("EASY");
+     }
+}
